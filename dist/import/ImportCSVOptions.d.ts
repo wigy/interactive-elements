173695fdf44012c115c11519e971d945de7e8f09
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2021-12-29 17:08:13 +0200

    More CSV options.

--- dist/import/ImportCSVOptions.d.ts
+++ dist/import/ImportCSVOptions.d.ts
@@ -2,8 +2,13 @@
  * Options for the CSV parsing.
  *
  * * `useFirstLineHeadings` - If set, the first line is first trimmed from space an # and then used as headings for columns.
+ * * `cutFromBeginning` - Drop this many lines from the start.
+ * * `columnSeparator` - A column seprator string. Defaults to `,`.
+ * * `trimLines` - If set, trim white space from lines before using.
  */
 export declare type ImportCSVOptions = {
-    useFirstLineHeadings?: boolean;
     columnSeparator?: string;
+    cutFromBeginning?: number;
+    trimLines?: boolean;
+    useFirstLineHeadings?: boolean;
 };
--- src/import/ImportCSVOptions.ts
+++ src/import/ImportCSVOptions.ts
@@ -2,8 +2,13 @@
  * Options for the CSV parsing.
  *
  * * `useFirstLineHeadings` - If set, the first line is first trimmed from space an # and then used as headings for columns.
+ * * `cutFromBeginning` - Drop this many lines from the start.
+ * * `columnSeparator` - A column seprator string. Defaults to `,`.
+ * * `trimLines` - If set, trim white space from lines before using.
  */
-export type ImportCSVOptions = {
-  useFirstLineHeadings?: boolean,
+ export declare type ImportCSVOptions = {
   columnSeparator?: string
+  cutFromBeginning?: number
+  trimLines?: boolean
+  useFirstLineHeadings?: boolean
 }
