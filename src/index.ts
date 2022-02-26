52398e56e18e9d84d3dfd586e0b683cfa5641429
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2022-02-26 12:54:46 +0200

    Add todo.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 /**
- * Type definitions and some essential utilities for Interactive Stateful Processes.
+ * # Interactive Elements
+ *
+ * This module contains type definitions and some essential utilities for Interactive Stateful Processes.
+ * @todo Add link to the overview of the system.
+ *
  * @module
  */
 export * from './api_types'
