0a6a42d43026149b2e7c2514e0e79d1b894aa97a
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2022-03-06 12:00:20 +0200

    Update documentation.

--- src/import/index.ts
+++ src/import/index.ts
@@ -16,6 +16,16 @@
  *    execution --> DONE
  * ```
  *
+ * 1. Segmentation: Split incoming data to smaller pieces so that parts of original data are grouped
+ *    together under unique *segmentation ID*. Each group present some meaningful unit of information
+ *    belonging together called *segment*.
+ * 2. Classification: For each segment, determine which *class* it belongs to. Classes are some
+ *    defined set of information describing similar segments.
+ * 3. Analyze: Based on the classification, additional information can be collected and/or calculated
+ *    for each segment.
+ * 4. Execution: Once information for each segment is complete, we can execute database insertions,
+ *    REST API calls or whatever appropriate actions based on the segement data.
+ *
  * @module interactive-elements/src/import
  */
 export * from './ImportAction'
