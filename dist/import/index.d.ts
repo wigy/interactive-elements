5c5d0120072dfac6e3b871fa5e9c86a3e7da6fce
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2022-03-06 12:36:38 +0200

    Compile docs.

--- dist/import/index.d.ts
+++ dist/import/index.d.ts
@@ -13,17 +13,17 @@
  *    segmentation --> classification
  *    classification --> analyze
  *    analyze --> execution
- *    execution --> DONE
+ *    execution --> END
  * ```
  *
- * 1. Segmentation: Split incoming data to smaller pieces so that parts of original data are grouped
+ * 1. **Segmentation**: Split incoming data to smaller pieces so that parts of the original data are grouped
  *    together under unique *segmentation ID*. Each group present some meaningful unit of information
  *    belonging together called *segment*.
- * 2. Classification: For each segment, determine which *class* it belongs to. Classes are some
- *    defined set of information describing similar segments.
- * 3. Analyze: Based on the classification, additional information can be collected and/or calculated
+ * 2. **Classification**: For each segment, determine which *class* it belongs to. Classes are some
+ *    defined set of qualities describing similar segments.
+ * 3. **Analyze**: Based on the classification, additional information can be collected and/or calculated
  *    for each segment.
- * 4. Execution: Once information for each segment is complete, we can execute database insertions,
+ * 4. **Execution**: Once information for each segment is complete, we can execute database insertions,
  *    REST API calls or whatever appropriate actions based on the segement data.
  *
  * @module interactive-elements/src/import
