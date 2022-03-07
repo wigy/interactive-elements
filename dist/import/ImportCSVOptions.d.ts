d756480af4ddeba8e58b4678ae5e0fd557cf5306
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2022-03-07 12:24:09 +0200

    Add skipErrors for CSV options.

--- dist/import/ImportCSVOptions.d.ts
+++ dist/import/ImportCSVOptions.d.ts
@@ -5,10 +5,12 @@
  * * `cutFromBeginning` - Drop this many lines from the start.
  * * `columnSeparator` - A column seprator string. Defaults to `,`.
  * * `trimLines` - If set, trim white space from lines before using.
+ * * `skipErrors` - If set, ignore lines with errors.
  */
 export declare type ImportCSVOptions = {
     columnSeparator?: string;
     cutFromBeginning?: number;
     trimLines?: boolean;
     useFirstLineHeadings?: boolean;
+    skipErrors?: boolean;
 };
